0c1d025f1848040c8d8f0cae1b19bf71afee17df
baumandal1415 <baumandal1415@gmail.com>
2021-07-14 00:51:05 +0600

    undefined-vs-null

---
+++ undefined-vs-null.js
@@ -0,0 +1,24 @@
+// let pakhi;
+
+// function add(num1, num2) {
+//     console.log(num1 + num2);
+// }
+
+// function add(num1, num2) {
+//     console.log(num1 + num2);
+//     return
+// }
+
+// function add(num1, num2) {
+//     console.log(num1, num2);
+// }
+
+// const result = add(12);
+
+// const premik = { Name: 'cool dude', phone: 3740874 }
+
+// let fun = undefined;
+
+const number = [1, 2, 3];
+
+console.log(number[11]);
