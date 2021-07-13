0880f03dabaca2430636a6b28fda8be29af75071
baumandal1415 <baumandal1415@gmail.com>
2021-07-14 01:03:14 +0600

    double-vs-triple-equal

--- undefined-vs-null.js
+++ undefined-vs-null.js
@@ -1,23 +1,23 @@
-// let pakhi;
+let pakhi;
 
-// function add(num1, num2) {
-//     console.log(num1 + num2);
-// }
+function add(num1, num2) {
+    console.log(num1 + num2);
+}
 
-// function add(num1, num2) {
-//     console.log(num1 + num2);
-//     return
-// }
+function add(num1, num2) {
+    console.log(num1 + num2);
+    return
+}
 
-// function add(num1, num2) {
-//     console.log(num1, num2);
-// }
+function add(num1, num2) {
+    console.log(num1, num2);
+}
 
-// const result = add(12);
+const result = add(12);
 
-// const premik = { Name: 'cool dude', phone: 3740874 }
+const premik = { Name: 'cool dude', phone: 3740874 }
 
-// let fun = undefined;
+let fun = undefined;
 
 const number = [1, 2, 3];
 
