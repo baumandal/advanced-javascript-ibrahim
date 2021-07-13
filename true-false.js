1908ebd8d32cf173b33b94f1365abf6a0813ce26
baumandal1415 <baumandal1415@gmail.com>
2021-07-14 00:36:32 +0600

    true-false

---
+++ true-false.js
@@ -0,0 +1,24 @@
+// falsy
+
+// 0
+// ''
+// undefined
+// null
+// NaN
+// false
+
+// truthy
+
+// " "
+// '0'
+// []
+// "false"
+
+
+let Name = false
+
+if (Name) {
+    console.log('condition is true');
+} else {
+    console.log('condition is false');
+}
